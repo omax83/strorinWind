1c7a8fb311a3eb719ae7d92aba937dfdce7eff9f
omax83 <omaxik@gmail.com>
2017-06-12 00:38:20 +0300

    Create app.py

--- app.py
+++ app.py
@@ -17,23 +17,40 @@ import os
 from flask import Flask
 from flask import request
 from flask import make_response
+import vk
 
 # Flask app should start in global layout
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask2!'
+    return 'Hello from Flask3!'
 @app.route('/', methods=['POST'])
 def processing():
+    #Распаковываем json из пришедшего GET-запроса
     data = json.loads(request.data)
+    #Вконтакте в своих запросах всегда отправляет поле типа
     if 'type' not in data.keys():
         return 'not vk'
     if data['type'] == 'confirmation':
         return confirmation_token
     elif data['type'] == 'message_new':
-        messageHandler.create_answer(data['object'], token)
-    return 'ok'
+        session = vk.Session()
+        api = vk.API(session, v=5.0)
+        user_id = data['object']['user_id']
+        api.messages.send(access_token=token, user_id=str(user_id), message='Привет, я новый бот!')
+        # Сообщение о том, что обработка прошла успешно
+        return 'ok'
+#@app.route('/', methods=['POST'])
+#def processing():
+#    data = json.loads(request.data)
+#    if 'type' not in data.keys():
+#        return 'not vk'
+#    if data['type'] == 'confirmation':
+#        return confirmation_token
+#    elif data['type'] == 'message_new':
+#        messageHandler.create_answer(data['object'], token)
+#    return 'ok'
 #@app.route('/', methods=['POST'])
 #def processing():
 #    return '13507e18'
