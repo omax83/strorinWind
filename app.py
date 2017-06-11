e6cdbd1eb63998075b66966b6a66886b45e30ba9
omax83 <omaxik@gmail.com>
2017-06-11 16:16:18 +0300

    Create app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
-
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
 import json
 import os
 
