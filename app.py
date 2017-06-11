bd1ee74a6007fbf5a92711d27b51156c6b398e91
omax83 <omaxik@gmail.com>
2017-06-12 00:41:53 +0300

    Create app.py

--- app.py
+++ app.py
@@ -135,9 +135,9 @@ def makeWebhookResult(data):
     }
 
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print("Starting app on port %d" % port)
-
-    app.run(debug=False, port=port, host='0.0.0.0')
+#if __name__ == '__main__':
+#    port = int(os.getenv('PORT', 5000))
+#
+#    print("Starting app on port %d" % port)
+#
+#    app.run(debug=False, port=port, host='0.0.0.0')
